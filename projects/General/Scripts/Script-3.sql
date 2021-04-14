1006525a117ad64861192e8198edc13986d3ddcc
nichez <nikola.panev93@gmail.com>
2021-04-14 22:01:58 +0200

    remove wrong database file

--- projects/General/Scripts/Script-3.sql
+++
@@ -1,9 +0,0 @@
-USE testExpress
-
--- INSERT INTO `courses` (`Id`, `title`, `studentId`) VALUES
--- ('11', 'Course One', '1');
-
--- INSERT INTO `students` (`Id`, `Name`) VALUES
--- ('1', 'John McDonald');
-
-SELECT * from courses
